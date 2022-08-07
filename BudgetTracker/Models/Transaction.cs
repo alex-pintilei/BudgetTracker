d7e8a2749d7d8b0147e410e233ee2d91f6fddf5a
alex-pintilei <pintileialexandru@yahoo.com>
2022-08-07 17:22:37 +0300

    1s : Setting up the Models and creation of Database (AppDbContext)

---
+++ BudgetTracker/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BudgetTracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; } 
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } // navigation property
+
+        public float Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(85)")]
+        public string? Note { get; set; }
+    }
+}
